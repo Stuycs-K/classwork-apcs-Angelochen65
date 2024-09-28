d0118f9a8f994b6cb969c1b7bb5e3121468a6802
Angelo Chen <achen65@stuy.edu>
2024-09-28 17:26:37 -0400

    Forgot ;

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -8,7 +8,7 @@ public class MyArrays {
 
   }
   public static void main(String[] args){
-    int[] test = new int[] {1,2,3,4,5}
+    int[] test = new int[] {1,2,3,4,5};
     System.out.println("Are the arrays equal?" + test == returnCopy(test));
     test[0] = 3;
     System.out.println("Are the arrays the same?" + test == returnCopy(test));
