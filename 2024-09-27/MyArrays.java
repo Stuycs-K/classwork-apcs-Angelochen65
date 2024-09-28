5a5d697415c21283559510e830a17ac2050d0785
Angelo Chen <achen65@stuy.edu>
2024-09-28 17:37:23 -0400

    aryToString

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,4 +1,15 @@
 public class MyArrays {
+  public static String aryToString (int[]ary){
+    String end = "[";
+    for(int i = 0; i < ary.length; i++){
+      end+= ary[i];
+      if(i != ary.length -1){
+        end+=", "
+      }
+    }
+    end += "]";
+    return end;
+  }
   public static int[] returnCopy (int[]ary){
     int[] newAry = new int[ary.length];
     for (int i = 0; i < ary.length; i++){
@@ -10,8 +21,8 @@ public class MyArrays {
   public static void main(String[] args){
     int[] test = new int[] {1,2,3,4,5};
 
-    System.out.print("Are the arrays equal?");
-    System.out.print(test == returnCopy(test));
-  
+    System.out.print("Are the arrays equal ?");
+    System.out.println(test == returnCopy(test));
+    System.out.print(aryToString(test));
   }
 }
