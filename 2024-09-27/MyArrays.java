fdba9bf2c92cf51695aff4195154adc39b0e504f
Angelo Chen <achen65@stuy.edu>
2024-09-28 18:01:18 -0400

    Checking out test

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -18,6 +18,8 @@ public class MyArrays {
     return newAry;
 
   }
+
+  public static int[] concatArray (int[]ary1, int[]ary2)
   public static void main(String[] args){
     int[] test = new int[] {1,2,3,4,5};
     System.out.println("Expected: " + aryToString(test) + " actual: " + aryToString(returnCopy(test)));
@@ -27,6 +29,13 @@ public class MyArrays {
     System.out.println("Expected: " + aryToString(test) + " actual: " + aryToString(returnCopy(test)));
     System.out.print("Are the arrays the same? ");
     System.out.println(test == returnCopy(test));
+
+    int[] test1 = new int[] {1, 3, 5};
+    int[] test2 = new int[] {6, 8, 10};
+    System.out.println("Expected: " +
+      (aryToString(test1)).substring(0, test1.length - 1) +
+      (aryToString(test2).substring(1)));
+
 
   }
 }
