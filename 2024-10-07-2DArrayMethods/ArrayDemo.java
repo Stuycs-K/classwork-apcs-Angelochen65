f9351b7e77c12ae71c1e99d3790e3185d80c59dc
Angelo Chen <achen65@stuy.edu>
2024-10-07 21:59:52 -0400

    Trying to see the difference between the 2d Arrays

--- 2024-10-07-2DArrayMethods/ArrayDemo.java
+++ 2024-10-07-2DArrayMethods/ArrayDemo.java
@@ -14,6 +14,8 @@ public class ArrayDemo{
 
     System.out.println(Arrays.toString(arr1).equals(arrToString(arr1)));
     System.out.println(Arrays.toString(arr2).equals(arrToString(arr2)));
+    System.out.println(Arrays.toString(arr2));
+    System.out.println(arrToString(arr2));
 
   }
 
