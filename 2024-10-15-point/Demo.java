f86189ca1330a22ffe9abe5439506d8a18b09662
Angelo Chen <achen65@stuy.edu>
2024-10-15 23:40:26 -0400

    Tried another triangle point

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -24,7 +24,7 @@ public class Demo{
 
     Point tri1 = new Point(0,0);
     Point tri2 = new Point(1,0);
-    Point tri3 = new Point(0.5, Math.sqrt(1-(0.5*0.5)));
+    Point tri3 = new Point(0.5, Math.sqrt(0.75));
     System.out.println(distance(tri1,tri2));
     System.out.println(distance(tri2,tri3));
     System.out.println(distance(tri1,tri3));
