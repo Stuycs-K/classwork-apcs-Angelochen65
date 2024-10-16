9d878c4b197e17b34e1cc6496d4fca8e1e7cfd00
Angelo Chen <achen65@stuy.edu>
2024-10-15 23:27:38 -0400

    Copied and Pasted

---
+++ 2024-10-15-point/Demo.java
@@ -0,0 +1,23 @@
+public class Demo{
+
+  /*
+  Write this method third. Test it then move on
+  to the others.
+  */
+  public static double distance(Point a, Point b){
+    return Math.sqrt(Math.pow(b.getY() - a.getY(),2) + Math.pow(b.getX() - a.getX(),2));
+  }
+
+  public static void main(String[]args){
+    Point p1 = new Point(1,1);
+    Point p2 = new Point(-1,-1);
+    Point p3 = new Point(3,4);
+    System.out.println( p3);
+    System.out.println( distance(p1,p2));
+    System.out.println( Point.distance(p1,p2));
+    System.out.println( p1.distanceTo(p2));
+    System.out.println(distance(p1,p2)==Point.distance(p1,p2)&&Point.distance(p1,p2)==p1.distanceTo(p2));
+    System.out.println(distance(p1,p3)==Point.distance(p1,p3)&&Point.distance(p1,p3)==p1.distanceTo(p3));
+    System.out.println(distance(p2,p3)==Point.distance(p2,p3)&&Point.distance(p2,p3)==p2.distanceTo(p3));
+  }
+}
