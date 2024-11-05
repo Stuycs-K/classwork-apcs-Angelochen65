173f2599c27e8258f339e4005de983139b71af2a
Angelo Chen <achen65@stuy.edu>
2024-11-04 19:10:23 -0500

    finished countTriangles A

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,45 +1,38 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+public class TriangleTester {
 
-public class TriangleTester{
-  public static void main(String[] args){
-
+  public static void main(String[] args) {
+      // Call countTrianglesA with the input file and print the result
+      System.out.println(countTrianglesA("inputTri.txt"));
   }
 
-  public static int countTrianglesA(String filename){
+  public static boolean valTriangle(int a, int b, int c) {
+    return (a + b > c) && (a + c > b) && (b + c > a);
+    }
+
+    // Main function to count the number of valid triangles in the file
+  public static int countTrianglesA(String filename) {
     int total = 0;
-    Scanner input = new Scanner(filename);
-    while(input.hasNextLine){
-      for(int i = 1; i <= 3; i++){
-
-      }
-
-      }
+    try {
+      Scanner input = new Scanner(new File(filename));
+      while (input.hasNextLine()) {
+          int a = input.nextInt();
+          int b = input.nextInt();
+          int c = input.nextInt();
+          if (valTriangle(a, b, c)) {
+            total++; }
+            if (input.hasNextLine()) {
+              input.nextLine();
+            }
+          }
+          input.close();
+        } catch (FileNotFoundException e) {
+          System.out.println("File not found: " + filename);
+        }
+        return total;
     }
-
-    return total;
-
-  }
-
-  public static boolean valTriangle(int a, int b, in c){
-    if(a > b && a > c){
-      return (a < b + c);
-    }
-    if(b > a && b > c){
-      return (b < a + c);
-    }
-    if(c > b && c > a){
-      return (c < b + a);
-    }
-  }
-
-
-
-
-
-
-
 
 
 }
