c2a671cce20450385e0f5f8996e1c22b99cd2b91
Angelo Chen <achen65@stuy.edu>
2024-11-19 00:47:02 -0500

    Yay! We did it by fixing the helper function.

--- 2024-11-18-advent2016/day1part1.java
+++ 2024-11-18-advent2016/day1part1.java
@@ -20,8 +20,6 @@ public class day1part1{
 
        ArrayList<String> pos = new ArrayList<String>();
        pos.add("north");
-       System.out.println(pos.get(0));
-       System.out.println(pos.get(0).equals("north"));
 
       try {
         Scanner input = new Scanner(new File(filename));
@@ -33,7 +31,7 @@ public class day1part1{
           String nextToken= input.next();
 
           changeDirection(pos, nextToken.substring(0,1));
-          int dist = Integer.parseInt(nextToken.substring(1));
+          int dist = Integer.parseInt(nextToken.substring(1).trim());
             if(pos.get(0).equals("north")){
               vert+= dist;
             }
@@ -56,35 +54,30 @@ public class day1part1{
           return Math.abs(vert)+Math.abs(hori);
         }
 
-        public static void changeDirection(ArrayList<String> arr, String b){
-          if(arr.get(0).equals("north") && b.equals("R")){
-            arr.set(0, "east");
-          }
-          else if(arr.get(0).equals("north") && b.equals("L")){
-            arr.set(0, "west");
-          }
+    public static void changeDirection(ArrayList<String> arr, String b){
+        String currentDirection = arr.get(0);
 
-          if(arr.get(0).equals("east") && b.equals("R")){
-            arr.set(0, "south");
-          }
-          else if(arr.get(0).equals("east") && b.equals("L")){
-            arr.set(0, "north");
-          }
-          if(arr.get(0).equals("south") && b.equals("R")){
-            arr.set(0, "west");
-          }
-          else if(arr.get(0).equals("north") && b.equals("L")){
+        if (currentDirection.equals("north") && b.equals("R")) {
             arr.set(0, "east");
-          }
-          if(arr.get(0).equals("west") && b.equals("R")){
-            arr.set(0, "north");
-          }
-          else if(arr.get(0).equals("west") && b.equals("L")){
+        } else if (currentDirection.equals("east") && b.equals("R")) {
             arr.set(0, "south");
-          }
-
+        } else if (currentDirection.equals("south") && b.equals("R")) {
+            arr.set(0, "west");
+        } else if (currentDirection.equals("west") && b.equals("R")) {
+            arr.set(0, "north");
         }
 
-
-
+        if (currentDirection.equals("north") && b.equals("L")) {
+            arr.set(0, "west");
+        } else if (currentDirection.equals("east") && b.equals("L")) {
+            arr.set(0, "north");
+        } else if (currentDirection.equals("south") && b.equals("L")) {
+            arr.set(0, "east");
+        } else if (currentDirection.equals("west") && b.equals("L")) {
+            arr.set(0, "south");
+        }
     }
+
+
+
+}
