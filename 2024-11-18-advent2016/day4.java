26f94e8b44a6964c7a6ff5b75f0ef628cc056c9f
Angelo Chen <achen65@stuy.edu>
2024-11-20 10:14:55 -0500

    day4 making file

---
+++ 2024-11-18-advent2016/day4.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class day4{
+  public static void main(String[] args){
+
+
+
+
+  }
+
+  public static int realRoom(String filename){
+
+  }
+  /*
+  [a, b, c]
+  [a.count, b.count, c.count]
+
+  */
+
+
+
+
+
+}
