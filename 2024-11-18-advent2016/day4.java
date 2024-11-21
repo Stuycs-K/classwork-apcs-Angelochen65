ad4fae33bea844ff768d7668c0f3f91ebf4625aa
Angelo Chen <achen65@stuy.edu>
2024-11-20 19:18:15 -0500

    Each room into an index on arrayList

--- 2024-11-18-advent2016/day4.java
+++ 2024-11-18-advent2016/day4.java
@@ -1,9 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class day4{
   public static void main(String[] args){
+    System.out.println(realRoom("room.txt"));
 
 
 
@@ -11,6 +13,21 @@ public class day4{
   }
 
   public static int realRoom(String filename){
+    ArrayList<String> rooms = new ArrayList<String>();
+
+    try {
+        Scanner input = new Scanner(new File(filename));
+        while (input.hasNextLine()) {
+          rooms.add(input.nextLine());
+
+
+        }
+        input.close();
+    } catch (FileNotFoundException e) {
+        System.out.println("File not found: " + filename);
+    }
+    System.out.println(rooms);
+    return 0;
 
   }
   /*
