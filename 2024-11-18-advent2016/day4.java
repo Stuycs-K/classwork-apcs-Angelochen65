82cd99ac9c585fa75588eb527264ded93d78db64
Angelo Chen <achen65@stuy.edu>
2024-11-20 19:41:01 -0500

    Added how many times each letter occurs in the room

--- 2024-11-18-advent2016/day4.java
+++ 2024-11-18-advent2016/day4.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class day4{
   public static void main(String[] args){
@@ -14,6 +15,9 @@ public class day4{
 
   public static int realRoom(String filename){
     ArrayList<String> rooms = new ArrayList<String>();
+    ArrayList<Character> repeats = new ArrayList<Character>();
+    ArrayList<Integer> nums = new ArrayList<Integer>();
+    String[] letters;
 
     try {
         Scanner input = new Scanner(new File(filename));
@@ -26,7 +30,27 @@ public class day4{
     } catch (FileNotFoundException e) {
         System.out.println("File not found: " + filename);
     }
-    System.out.println(rooms);
+    for(int i = 0; i < 1; i++){
+      letters = rooms.get(i).split("-");
+      System.out.println(Arrays.toString(letters));
+      for(int a = 0; a < letters.length-1; a++){
+        for(int b = 0; b < letters[a].length(); b++){
+          if(repeats.indexOf(letters[a].charAt(b))<0){
+            repeats.add(letters[a].charAt(b));
+            nums.add(1);
+          }
+          else{
+            nums.set(repeats.indexOf(letters[a].charAt(b)), nums.get(repeats.indexOf(letters[a].charAt(b)))+1);
+          }
+        }
+
+      }
+
+
+    }
+    System.out.println(repeats);
+    System.out.println(nums);
+    // System.out.println(rooms);
     return 0;
 
   }
