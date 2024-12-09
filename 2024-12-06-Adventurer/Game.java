1a86633f71c87993b04ec6ea3d742cc0f3787d60
Angelo Chen <achen65@stuy.edu>
2024-12-09 10:11:11 -0500

    loop for action

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 public class Game{
   public static void main(String[] args) {
+    Adventurer Player = new Reaper("Star", 50, 3);
+    Adventurer Enemy = new CodeWarrior("Anaconda", 50, "python");
 
     //do this once
     Scanner userInput = new Scanner(System.in);
@@ -12,5 +14,12 @@ public class Game{
     String userName = userInput.nextLine();
     //Do something with the input
     System.out.println("Username is: " + userName);
+
+    String action = userInput.nextLine();
+
+    while(!action.equals("quit")){
+      System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+      action = userInput.nextLine();
+    }
   }
 }
